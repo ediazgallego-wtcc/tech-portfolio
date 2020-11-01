ffb731a82923a16327a8732efe4d8a44a76849b3
ediazgallego-wtcc <ediazgallego@my.waketech.edu>
2020-11-01 13:42:16 -0500

    firing actions

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -13,7 +13,7 @@ const users = [];
 
 const siteConfig = {
     title: 'Esteban Diaz Gallego', // Title for your website.
-    tagline: 'Cloud and Cognitive Software Developer',
+    tagline: 'Cloud and Cognitive Software Developer, Soccer Lover',
     profilePhoto: 'img/profile.png',
     url: 'https://ediazgallego-wtcc.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
